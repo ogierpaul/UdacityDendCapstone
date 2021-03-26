521a9c00642ca1e583f5bb58504d3ec169a73380
ogierpaul <ogier.paul@gmail.com>
2021-03-26 19:54:31 +0100

    Fixed bug on siren name

--- dags/siren/transformation/schema_siren_dwh.sql
+++ dags/siren/transformation/schema_siren_dwh.sql
@@ -1,18 +1,18 @@
 CREATE OR REPLACE VIEW datalake.siren_standardized AS
 SELECT siren,
-       COALESCE(nomUniteLegale, '') || COALESCE(nomUsageUniteLegale, '') || COALESCE(denominationUniteLegale, '') ||
-       COALESCE(denominationUsuelle1UniteLegale, '') as name,
+       'FR' || LPAD( CAST(MOD(12 + 3 * MOD(CAST(siren AS INTEGER), 97), 97) AS VARCHAR), 2, '0')  || siren as euvat,
+       COALESCE(nomUniteLegale, '') || COALESCE(nomUsageUniteLegale, '') || COALESCE(denominationUniteLegale, '') || COALESCE(denominationUsuelle1UniteLegale, '') as sirenname,
        economieSocialeSolidaireUniteLegale,
        trancheEffectifsUniteLegale,
        categorieentreprise
--- 'FR' || LPAD( CAST(MOD(12 + 3 * MOD(CAST(siren AS INTEGER), 97), 97) AS VARCHAR), 2, '0')  || siren as euvat
 FROM datalake.siren_attributes;
 
 
 CREATE TABLE IF NOT EXISTS dwh.siren_attributes
 (
     siren                               VARCHAR,
-    name                                VARCHAR,
+    euvat                               VARCHAR,
+    sirenname                           VARCHAR,
     economieSocialeSolidaireUniteLegale VARCHAR,
     trancheEffectifsUniteLegale         VARCHAR,
     categorieentreprise                 VARCHAR
